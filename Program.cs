0c3086f5ee633478e046180d26bb35ee8e24cc32
lourencofelipe <felipe.pucsc@gmail.com>
2022-07-15 15:49:28 -0300

    Adding Get Endpoints

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI.Core.Data;
+using MinimalAPI.Core.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MinimalContextDb>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-	  
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,5 +21,27 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/provider", async (
+	MinimalContextDb context) =>
+
+	await context.Providers.ToListAsync())
+	.WithName("GetProviders")
+	.WithTags("Provider");
+
+
+app.MapGet("/provider/{id}", async (
+	Guid id,
+	MinimalContextDb context) =>
+
+	await context.Providers.FindAsync(id)
+		is Provider provider
+			? Results.Ok(provider)
+			: Results.NotFound())
+	.Produces<Provider>(StatusCodes.Status200OK)
+	.Produces(StatusCodes.Status404NotFound)
+	.WithName("GetProviderById")
+	.WithTags("Provider");
+
 
 app.Run();
